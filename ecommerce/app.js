7b77524345605e04c6b211a7f587c5f523e73c51
Anshu Singh <anshusingh0284087@gmail.com>
2025-07-31 19:27:18 +0530

    Initial commit

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -1,21 +1,38 @@
+// const express = require("express");
+// const app = express();
+
+// // Import route files
+// const userRoutes = require("./routes/userRoutes");
+// const productRoutes = require("./routes/productRoutes");
+// const cartRoutes = require("./routes/cartRoutes");
+
+// // Middleware
+// app.use(express.json());
+
+// // Route mounting
+// app.use("/users", userRoutes);
+// app.use("/products", productRoutes);
+// app.use("/cart", cartRoutes);
+
+// // Server setup
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+// server.js
 const express = require("express");
 const app = express();
+const PORT = 3000;
 
-// Import route files
-const userRoutes = require("./routes/userRoutes");
-const productRoutes = require("./routes/productRoutes");
-const cartRoutes = require("./routes/cartRoutes");
-
-// Middleware
+// Middleware to parse JSON
 app.use(express.json());
 
-// Route mounting
-app.use("/users", userRoutes);
-app.use("/products", productRoutes);
-app.use("/cart", cartRoutes);
+// Import routes
+const productRoutes = require("./routes/productRoutes");
 
-// Server setup
-const PORT = 3000;
+// Use routes
+app.use("/products", productRoutes);
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
